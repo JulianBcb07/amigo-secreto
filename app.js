d72874c8e6fe60984c627a62687b43e9e02ef195
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-08-27 00:58:06 -0600

    crear funcion agregar amigo y friendList

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// global variables
+let arrayFriends = [];
+
+listElement = document.getElementById("listaAmigos");
+console.log(listElement);
+
+// function called on HTML file
+const agregarAmigo = () => {
+  const friendValue = document.getElementById("amigo");
+  if (friendValue.value === null || friendValue.value === "") {
+    alert("Porfavor asegurese de no dejar el campo vacío");
+  } else {
+    console.log(friendValue.value);
+    arrayFriends.push(friendValue.value);
+    friendValue.value = "";
+    console.log(arrayFriends);
+  }
+};
+
+const friendList = () => {
+  for (const friend of arrayFriends) {
+    console.log(friend);
+    liElement = document.createElement("li");
+    liElement.innerText = friend;
+    listElement.append(liElement);
+  }
+};
