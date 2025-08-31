89c40de75c82c6b59ae0e0b5d4957b056b145100
Julian Daniel Bacab Chan <julianbacab.dc7@gmail.com>
2025-08-30 22:59:26 -0600

    crear funcion sortear amigo secreto

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 let arrayFriends = [];
 
 listElement = document.getElementById("listaAmigos");
+resultList = document.getElementById("resultado");
+buttonSortear = document.getElementById("button-sortear");
 console.log(listElement);
+console.log(resultList);
+console.log(buttonSortear);
 
 // function called on HTML file
 const agregarAmigo = () => {
@@ -14,14 +18,32 @@ const agregarAmigo = () => {
     arrayFriends.push(friendValue.value);
     friendValue.value = "";
     console.log(arrayFriends);
+    friendList();
   }
 };
 
 const friendList = () => {
-  for (const friend of arrayFriends) {
-    console.log(friend);
+  liElement = document.createElement("li");
+  liElement.style.margin = "15px";
+  listElement.style.fontSize = "1.5rem";
+  liElement.innerText = arrayFriends[arrayFriends.length - 1];
+  listElement.append(liElement);
+};
+
+// obtain the lenght of array
+
+const sortearAmigo = () => {
+  if (arrayFriends.length <= 0) {
+    alert("No puede sortear con la lista vacía!");
+  } else {
+    sorteo = Math.floor(Math.random() * arrayFriends.length);
     liElement = document.createElement("li");
-    liElement.innerText = friend;
-    listElement.append(liElement);
+    liElement.style.margin = "15px";
+    liElement.style.fontSize = "1.5rem";
+    liElement.style.fontWeight = "600";
+    liElement.innerText = `El amigo secreto es: ${arrayFriends[sorteo]}`;
+    resultList.append(liElement);
+    buttonSortear.setAttribute("disabled", "true");
+    buttonSortear.style.background = "gray";
   }
 };
